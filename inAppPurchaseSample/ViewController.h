15acc2e18ba658c5b1a01346a946afd7c71f1dc9
photondragon <photondragon@163.com>
2016-05-01 00:50:02 +0800

    Initial Commit

---
+++ inAppPurchaseSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  inAppPurchaseSample
+//
+//  Created by photondragon on 16/5/1.
+//  Copyright © 2016年 mahu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
